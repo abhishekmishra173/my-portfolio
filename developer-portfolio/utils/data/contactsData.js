4d83344ee76b12f78efe688c4e1e17120b5fb588
abhishekmishra173 <innocentabhishek173@gmail.com>
2025-11-10 17:18:22 +0530

    Clean final upload of all portfolio files

---
+++ developer-portfolio/utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+  email: "mishra.abhishek0703@gmail.com",
+  phone: "+91 9209672442",
+  address: "Nashik ,Maharashtra, India - 422103",
+  github: "https://github.com/abhishekmishra173",
+  facebook: "https://www.facebook.com/abhishekmishra0703",
+  linkedIn: "https://www.linkedin.com/in/abhishek-mishra-65a951298",
+  twitter: "https://twitter.com/_abhishek173",
+  stackOverflow: "https://stackoverflow.com/users/31848677/abhishek-mishra",
+  devUsername:  "abhishek_mishra_39244395e",
+};
